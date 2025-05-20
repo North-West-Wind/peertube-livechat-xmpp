79602833698a36736b70a878acf4819fa8654538
North-West-Wind <wsyn148@gmail.com>
2025-05-20 11:56:08 +0800

    manager iterators

--- src/manager.ts
+++ src/manager.ts
@@ -1,16 +1,32 @@
 import { Element } from "@xmpp/xml";
 import EventEmitter from "events";
 
+/**
+ * Manager is a way to store an immutable map
+ * It simply does not provide functions for modifying the map
+ */
 export abstract class Manager<K, V> extends EventEmitter {
 	protected map = new Map<K, V>();
 
 	abstract handle(stanza: Element): void;
 
-	set(key: K, value: V) {
+	protected set(key: K, value: V) {
 		this.map.set(key, value);
 	}
 
 	get(key: K) {
 		return this.map.get(key);
+	}
+
+	keys() {
+		return this.map.keys();
+	}
+
+	values() {
+		return this.map.values();
+	}
+
+	entries() {
+		return this.map.entries();
 	}
 }
